48f663d667e585dc0f3d12d0b5ca72dbdd699741
adam <adam@adam-K53TK>
2014-11-01 13:39:32 -0400

    html template and new routes

--- lunch/controller.py
+++ lunch/controller.py
@@ -21,3 +21,23 @@ def yo():
 @app.route('/register')
 def register():
 	return render_template('register.html', app_id=app.config['FB_APP'])
+
+@app.route('/<user1>/wantsToLunch/<user2>/near/<latitude>/<longitude>')
+def wantsToLunch():
+	return user1 + ' wantsToLunch ' + user2 + ' near ' + latitude + ' ' + longitude
+
+@app.route('/<user1>/confirmsLunchWith/<user2>')
+def wantsToLunch():
+	return user1 + ' confirmsLunchWith ' + user2
+
+@app.route('/<user1>/lunches/<user2>')
+def wantsToLunch():
+	return user1 + ' lunches ' + user2
+
+@app.route('/<user1>/lunches/<user2>')
+def wantsToLunch():
+	return user1 + ' lunches ' + user2
+
+@app.route('/done')
+def wantsToLunch():
+	return 'done'
