ea9e25090339b4978fdca971a30f0ca5b585c95d
Charlie Li <git@vishwin.info>
2014-11-01 03:42:11 -0400

    Initial Facebook login
    Base case for new users: log in via Facebook, request reading of friends list

--- lunch/controller.py
+++ lunch/controller.py
@@ -1,5 +1,18 @@
 from lunch import app
+from flask import redirect, request
+import json
+import requests
+
+def send_yo(username, link):
+	requests.post('http://api.justyo.co/yo/', data={'api_token': app.config['YO_API'], 'username': username, 'link': link})
 
 @app.route('/')
 def tsst():
 	return 'Tsst!'
+
+@app.route('/yo')
+# Yo callback
+def yo():
+	username=request.args.get('username')
+	send_yo(username, 'https://www.facebook.com/dialog/oauth?client_id={0}&redirect_uri=https://www.facebook.com/connect/login_success.html&scope=user_friends'.format(app.config['FB_APP']))
+	return 'OK'
